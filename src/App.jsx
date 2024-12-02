6d320c30fe2e2592cb467512029c8c0703d4256c
Zongcheng Wang <zongchew@uci.edu>
2024-12-02 15:21:13 -0500

    122. Adding Header and Footer to React App

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import HelloWorld from "./HelloWorld";
 import ListTodoComponent from "./components/ListTodoComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <HeaderComponent />
       <ListTodoComponent />
+      <FooterComponent />
     </>
   );
 }
