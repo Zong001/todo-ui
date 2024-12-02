2c1b1bfe6f3626c94b0b395e92a5e48fa33e41b3
Zongcheng Wang <zongchew@uci.edu>
2024-12-02 14:41:29 -0500

    121. Connect React App with Get All Todos REST API(no arraw =>)

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_REST_API_URL = "http://localhost:8080/api/todos";
 
-// export function getAllTodos() {
-//   return axios.get(BASE_REST_API_URL);
-// }
+export function getAllTodos() {
+  return axios.get(BASE_REST_API_URL);
+}
 
-export const getAllTodos = () => axios.get(BASE_REST_API_URL);
+// export const getAllTodos = () => axios.get(BASE_REST_API_URL);
