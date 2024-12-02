0c4f7da010619853cc0ca95f39631355b7312924
Zongcheng Wang <zongchew@uci.edu>
2024-12-02 16:04:10 -0500

    123. Configure Routing in a React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import HelloWorld from "./HelloWorld";
+
 import ListTodoComponent from "./components/ListTodoComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <HeaderComponent />
-      <ListTodoComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* http://localhost:8080 */}
+          <Route path="/" element={<ListTodoComponent />}></Route>
+          {/* http://localhost:8080/todos */}
+          <Route path="/todos" element={<ListTodoComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
