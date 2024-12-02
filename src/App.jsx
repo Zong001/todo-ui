9c97b23cf2807f06ae3509a2ca1faeb4dd617855
Zongcheng Wang <zongchew@uci.edu>
2024-12-02 14:07:26 -0500

    120. Create ListTodoComponent + useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import HelloWorld from "./HelloWorld";
+import ListTodoComponent from "./components/ListTodoComponent";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>hello woud</h1>
-      <HelloWorld />
+      <ListTodoComponent />
     </>
   );
 }
---
+++ src/components/ListTodoComponent.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function ListTodoComponent() {
+  const dummyData = [
+    {
+      id: 1,
+      title: "Learn Spring Boot",
+      description: "Learn Spring boot with project",
+      completed: true,
+    },
+    {
+      id: 2,
+      title: "Learn Hibernate updated",
+      description: "Learn Hibernate with project updated",
+      completed: false,
+    },
+  ];
+  const [todos, setTodos] = useState(dummyData);
+  return (
+    <div className="container">
+      <h2 className="text-center">List of Todos</h2>
+      <div>
+        <table className="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>Todo Title</th>
+              <th>Todo Description</th>
+              <th>Todo Completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.title}</td>
+                <td>{todo.description}</td>
+                <td>{todo.completed ? "YES" : "NO"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
