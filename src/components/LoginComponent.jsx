4463a37b99d462c94fa3378d9637ae7155affead
Zongcheng Wang <zongchew@uci.edu>
2024-12-06 23:31:13 -0500

    163. Connect React App to Login REST API

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { loginAPICall } from "../services/AuthService";
+import { useNavigate } from "react-router-dom";
 
 const LoginComponent = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigator = useNavigate();
+
   function handleLoginForm(e) {
     e.preventDefault();
     const loginObj = { username, password };
     console.log(loginObj);
+
+    loginAPICall(loginObj)
+      .then((response) => {
+        console.log(response.data);
+        navigator("/todos");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return (
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -5,3 +5,6 @@ const AUTH_REST_API_BASE_URL = "http://localhost:8080/api/auth";
 export function registerAPICall(registerObj) {
   return axios.post(AUTH_REST_API_BASE_URL + "/register", registerObj);
 }
+
+export const loginAPICall = (loginObj) =>
+  axios.post(AUTH_REST_API_BASE_URL + "/login", loginObj);
