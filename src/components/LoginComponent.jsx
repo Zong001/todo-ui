c53d55ee35e446335b1ce3b33199212307267fd0
Zongcheng Wang <zongchew@uci.edu>
2024-12-08 01:11:09 -0500

    to compare

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -15,32 +15,15 @@ const LoginComponent = () => {
 
     loginAPICall(loginObj)
       .then((response) => {
-        // console.log(response.data);
+        console.log(response.data);
         const token =
           "Basic " +
           window.btoa(`${loginObj.usernameOrEmail}:${loginObj.password}`);
-        // console.log(token + "why");
-        // console.log(`${loginObj.usernameOrEmail}:${loginObj.password}`);
-        // const token =
-        //   "Basic " +
-        //   window.btoa(`${loginObj.usernameOrEmail}:${loginObj.password}`);
-        console.log(token);
-
-        // console.log(
-        //   "a " +
-        //     "Basic " +
-        //     window.btoa(`${loginObj.usernameOrEmail}:${loginObj.password}`)
-        // );
-
         storeToken(token);
         navigator("/todos");
       })
       .catch((error) => {
-        console.log("why");
-        console.error("Error response:", error.response?.data);
-
         console.error(error);
-        console.log("Login object:", loginObj); // 打印发出的数据
       });
   }
 
